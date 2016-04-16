e7ad3f1f7ac77216abd4827c93b6e939694389ff
talhaobject90 <talhaqqq@gmail.com>
2016-04-16 10:47:13 +0530

    after removing uniquness validation

--- app/models/producttype.rb
+++ app/models/producttype.rb
@@ -1,4 +1,4 @@
 class Producttype < ApplicationRecord
-  validates :typename, uniqueness: { message: 'A product type of same type exist' }
+   
   has_many :products
 end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,6 +1,5 @@
 class Supplier < ApplicationRecord
-  validates :companycode, uniqueness: { message: 'A Supplier with this company code exists' }
-  validates :chargecode, uniqueness: { message: 'A Supplier with this charge code exists' }
+  
 
   has_many :products
 end
