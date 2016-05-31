5ebfe216d1c5750ac74c0bdede91e21537dfb22d
talhaobject90 <talhaqqq@gmail.com>
2016-05-31 12:49:22 +0530

    after optional product brand

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
 
-  belongs_to :supplier
-  belongs_to :producttype
-  belongs_to :productbrand
+  belongs_to :supplier , optional: true
+  belongs_to :producttype , optional: true
+  belongs_to :productbrand  ,optional: true
   has_many :purchaseorderitems  , :dependent => :restrict_with_exception
   has_many :stockadjustmentitems , :dependent => :restrict_with_exception
   has_many :orderitems , :dependent => :restrict_with_exception
