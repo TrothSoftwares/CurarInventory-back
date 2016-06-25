3e88622f087ebb5b044dc10a878ad8c198855729
talhaobject90 <talhaqqq@gmail.com>
2016-06-25 16:17:30 +0530

    before changing databases

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,3 +1,4 @@
 class CustomerSerializer < ActiveModel::Serializer
     attributes :id ,:companyname , :companycode , :chargecode , :email , :address1, :address2, :suburb ,:city , :state, :country, :zipcode, :phone
+    has_many :orders
 end
