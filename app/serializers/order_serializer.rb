d08433c07b9a94c9bad0e808ff7aab77fb109d83
talhaqqq <talhaqqq@gmail.com>
2017-04-03 14:06:22 +0530

    for testing databse row llimit

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id ,:duedate ,:totalunits ,:totalcost , :orderstatus , :mrf , :location , :natureofwork , :issuancedate , :roundoff ,:chargableamount
+  attributes :id ,:duedate ,:totalunits ,:totalcost , :orderstatus , :mrf , :location , :natureofwork , :issuancedate , :roundoff ,:chargableamount ,:orderid
   belongs_to :customer
   belongs_to :supplier
   belongs_to :employee
@@ -7,5 +7,11 @@ class OrderSerializer < ActiveModel::Serializer
   belongs_to :approvedby
   has_many :orderitems
   has_many :stockadjustments
+
+
+
+  def orderid
+    object.id
+  end
 
 end
