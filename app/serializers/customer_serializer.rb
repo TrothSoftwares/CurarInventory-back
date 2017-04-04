5bf39e0d59929ce990cca7c0554e4a220e50affb
talhaqqq <talhaqqq@gmail.com>
2017-04-04 17:30:30 +0530

    before pushoing to heroku

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,5 +1,5 @@
 class CustomerSerializer < ActiveModel::Serializer
-    attributes :id ,:companyname , :companycode , :chargecode , :email , :address1, :address2, :suburb ,:city , :state, :country, :zipcode, :phone,:tin,:due
+    attributes :id ,:companyname , :companycode , :chargecode , :email , :address1, :address2, :suburb ,:city , :state, :country, :zipcode, :phone,:tin,:due 
     has_many :orders
     has_many :payments
 
